2eb78a496c83e944717a11cf1cd1e6ce776e192e
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-01-15 10:16:23 +0800

    Change copy path

--- nginx/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-work_dir=$(realpath -s $(dirname "$0"))
-
-docker build -t echizenryoma/nginx:stable ${work_dir}
--- squid/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-work_dir=$(realpath -s $(dirname "$0"))
-
-docker build -t echizenryoma/squid:3.5 ${work_dir}
